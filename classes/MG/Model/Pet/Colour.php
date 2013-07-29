2d210870beb9fb12b20757ac5a8efc17cbb86854
Hinton <hinton.oscar@gmail.com>
2013-07-29 14:24:38 +0200

    Fixed Formating.

--- classes/MG/Model/Pet/Colour.php
+++ classes/MG/Model/Pet/Colour.php
@@ -12,22 +12,22 @@ class MG_Model_Pet_Colour extends ORM {
 
 	protected $_table_columns = array(
 		'id'          => NULL,
-		'locked'       => NULL,
-		'name'    => NULL,
-		'description'    => NULL,
-		'image'      => NULL
+		'locked'      => NULL,
+		'name'        => NULL,
+		'description' => NULL,
+		'image'       => NULL
 	);
 
 	protected $_has_many = array(
-		'pets' => array(
-			'model' => 'User_Pet',
+		'pets'    => array(
+			'model'       => 'User_Pet',
 			'foreign_key' => 'colour_id',
 		),
 		'species' => array(
-			'model' => 'Pet_Specie',
-			'through' => 'pet_species_colours',
+			'model'       => 'Pet_Specie',
+			'through'     => 'pet_species_colours',
 			'foreign_key' => 'colour_id',
-			'far_key' => 'specie_id'
+			'far_key'     => 'specie_id'
 		)
 	);
 
--- classes/MG/Model/Pet/Specie.php
+++ classes/MG/Model/Pet/Specie.php
@@ -10,24 +10,24 @@
  */
 class MG_Model_Pet_Specie extends ORM {
 
-
 	protected $_table_columns = array(
 		'id'          => NULL,
-		'name'       => NULL,
-		'dir'    => NULL,
-		'description'    => NULL
+		'name'        => NULL,
+		'dir'         => NULL,
+		'description' => NULL
 	);
-		protected $_has_many = array(
-			'pets'    => array(
-				'model'       => 'User_Pet',
-				'foreign_key' => 'specie_id',
-			),
-			'colours' => array(
-				'through'     => 'pet_species_colours',
-				'model'       => 'Pet_Colour',
-				'foreign_key' => 'specie_id',
-				'far_key'     => 'colour_id'
-			)
-		);
 
-	}
+	protected $_has_many = array(
+		'pets'    => array(
+			'model'       => 'User_Pet',
+			'foreign_key' => 'specie_id',
+		),
+		'colours' => array(
+			'through'     => 'pet_species_colours',
+			'model'       => 'Pet_Colour',
+			'foreign_key' => 'specie_id',
+			'far_key'     => 'colour_id'
+		)
+	);
+
+}
